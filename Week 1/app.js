4e1f40387a8a8ff7e6e3896f9c2720a54c8df8a0
Alejandro Pérez <aleperez92@gmail.com>
2015-10-24 00:48:29 +0100

    Started week 1

--- Week 1/app.js
+++ Week 1/app.js
@@ -1,32 +1,42 @@
+'use-strict';
+
 var express = require('express'),
+    app = express(),
+    path = require('path'),
     cons = require('consolidate'),
     MongoClient = require('mongodb').MongoClient,
     Server = require('mongodb').Server;
 
-var app = express();
-
-var port = 8080;
-
 app.engine('html', cons.swig);
-
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 
-var mongoclient = new MongoClient(new Server("localhost", 27017));
-var db = mongoclient.db('m101');
+var mongoclient = new MongoClient(new Server('localhost', 27017));
+
+var db = mongoclient.db('course');
 
 app.get('/', function (req, res) {
-    db.collection('hw1_1').findOne({}, function (err, doc) {
-        res.render('hello', doc);
-    });
+
+    db.collection('test')
+        .findOne({}, function (err, doc) {
+
+            if (err) throw err;
+
+            res.render('hello', doc);
+        });
+
 });
 
-app.get('*', function(req, res){
-    res.send('Page not found', 404);
+app.get('*', function (req, res) {
+    res.status(404).send('Page not found');
 });
 
 mongoclient.open(function (err, mongoclient) {
+
     if (err) throw err;
-    app.listen(8080);
-    console.log('Express server running on localhost:' + port);
+
+    app.listen(8080, function () {
+        console.log('Server running on localhost:8080');
+    });
+
 });
