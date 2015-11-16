7902687a9006b1ede76c807f0ffb0a6e1a7587e7
Alejandro Pérez Martín <aleperez92@gmail.com>
2015-11-16 13:40:57 +0100

    Solved homework 5.1

---
+++ Week 5/homework_5.1.js
@@ -0,0 +1,20 @@
+
+db.posts.aggregate([
+    {
+        $unwind : "$comments"
+    },
+    {
+        $group: {
+            _id : "$comments.author",
+            "comments_count" : { $sum : 1 }
+        }
+    },
+    {
+        $sort: {
+            "comments_count" : -1
+        }
+    },
+    {
+        $limit : 1
+    }
+]);
